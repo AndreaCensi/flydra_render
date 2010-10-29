1f59a112fbe9feca04ad98ed79a409c1e5258f8c
Andrea Censi <andrea@cds.caltech.edu>
2010-10-29 11:35:08 -0700

    Added configuration example

--- src/rfsee/demo/demo_pipe.py
+++ src/rfsee/demo/demo_pipe.py
@@ -8,6 +8,10 @@ def main():
     
     # cp.config_stimulus_xml('/home/andrea/landing/20080626/4postsA.xml')
     cp.config_stimulus_xml(example_stim_xml)
+    
+    # 10x slower
+    # cp.config_compute_mu(True)
+    
     position = [0.5, 0.5, 0.5]
     attitude = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
     linear_velocity_body = [0, 0, 0]
