d64f9377f4d6ae533eb190408b5b2f1887408662
Andrea Censi <andrea@cds.caltech.edu>
2010-10-29 13:31:32 -0700

    simple benchmark

---
+++ src/fast-correlation/benchmark.py
@@ -0,0 +1,32 @@
+import numpy, time
+
+def corr_numpy(x):
+    C = numpy.dot(x.T, x)
+
+    return C
+
+def main():
+    
+    N = 10000
+    
+    x = numpy.random.rand(N, 1398)
+    
+    
+    functions = [('numpy', corr_numpy)]
+    
+    
+    for id, f in functions:
+        
+        start = time.time()
+        
+        C = f(x)
+        
+        print C.shape
+        T = time.time() - start
+        
+        print 'method: %15s  T: %s' % (id, T)
+
+
+
+if __name__ == '__main__':
+    main()
